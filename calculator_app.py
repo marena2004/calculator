5f0428fd5947ffde44d7579f3494b6782c269bb1
suparak <suparak.may@gmail.com>
2024-02-19 21:28:37 +0700

    calculator application updated

--- calculator_app.py
+++ calculator_app.py
@@ -1,10 +1,10 @@
 """module to run calculator application"""
-from view import CalculatorView
+from calculator_ui import CalculatorUI
 from controller import CalculatorController
 from model import CalculatorModel
 
 if __name__ == "__main__":
     model = CalculatorModel()
-    view = CalculatorView()
+    view = CalculatorUI()
     controller = CalculatorController(model, view)
     view.mainloop()
